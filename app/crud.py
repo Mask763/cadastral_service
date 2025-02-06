3fc7e9dd7cc27180f890174ffb8a4539a6ebd891
Максим Северин <max.vint@yandex.ru>
2025-02-06 17:33:59 +0400

    Update crud

--- app/crud.py
+++ app/crud.py
@@ -5,15 +5,19 @@ from app.models import Query
 from app.schemas import QueryCreateSchema
 
 
-async def creare_query(
+async def create_query(
     session: AsyncSession, query: QueryCreateSchema, response: bool
-):
+) -> Query:
     new_query = Query(**query.model_dump(), response=response)
     session.add(new_query)
+    await session.commit()
+    await session.refresh(new_query)
     return new_query
 
 
-async def get_queries_by_number(session: AsyncSession, number: str):
+async def get_queries_by_number(
+        session: AsyncSession, number: str
+) -> list[Query]:
     query = select(Query).where(Query.cadastral_number == number)
     result = await session.execute(query)
     return result.scalars().all()
@@ -21,7 +25,7 @@ async def get_queries_by_number(session: AsyncSession, number: str):
 
 async def get_all_queries(
     session: AsyncSession, skip: int = 0, limit: int = 10
-):
+) -> list[Query]:
     query = select(Query).offset(skip).limit(limit)
     result = await session.execute(query)
     return result.scalars().all()
