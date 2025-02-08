9b78f6da4e2f73098eda9c541966e9a357d04af4
Максим Северин <max.vint@yandex.ru>
2025-02-08 12:23:06 +0400

    Add mock server to docker compose

--- main_service/alembic/env.py
+++ main_service/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.models import Base
-from main_service.config import config as project_config
+from config import config as project_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- main_service/app/utils.py
+++ main_service/app/utils.py
@@ -3,11 +3,7 @@ from fastapi import HTTPException
 
 
 async def fetch_external_server_result():
-    try:
-        async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient() as client:
             # Отправляем запрос к эмулированному серверу
-            response = await client.get("http://127.0.0.1:8001/result")
-            response.raise_for_status()
+            response = await client.get("http://mock_server:8001/result")
             return response.json()["result"]
-    except httpx.RequestError as e:
-        raise HTTPException(status_code=500, detail=f"External server error: {str(e)}")
